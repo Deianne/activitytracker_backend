871e34b14e682063df21c97cb2e8ffeb160d6c5f
Anthony <asdfghjames09@gmail.com>
2025-04-23 12:02:48 +0800

    changes to the backend

--- activity_summarizer/activities.php
+++ activity_summarizer/activities.php
@@ -1,6 +1,6 @@
 <?php
 header("Access-Control-Allow-Origin: *"); // Allow all origins
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 header("Access-Control-Allow-Headers: Content-Type");
 
 // Handle preflight OPTIONS request
